fe7ebc7670f4572f3ed3eb60c8c1c5cd4217bbaf
Maslowski Lukasz <l.maslowski2012@gmail.com>
2021-10-28 23:53:43 +0200

    add HTML form validation, fix indentation

--- script.js
+++ script.js
@@ -1,101 +1,101 @@
 function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
 function addBookToLibrary() {
-    const form = document.getElementById('add-form').elements;
-    const formValues = [...form].filter(x => {
-      return x.localName === 'input' ? true : false;
-    });
+  const form = document.getElementById('add-form').elements;
+  const formValues = [...form].filter(x => {
+    return x.localName === 'input' ? true : false;
+  });
 
-    const title = formValues[0].value;
-    const author = formValues[1].value;
-    const pages = formValues[2].value;
-    const status = formValues[3].checked;
+  const title = formValues[0].value;
+  const author = formValues[1].value;
+  const pages = formValues[2].value;
+  const status = formValues[3].checked;
 
-    return new Book(title, author, pages, status);
+  return new Book(title, author, pages, status);
 }
 
 function initCards() {
-    const cardSection = document.getElementById('card-section');
-    myLibrary.forEach((element, index) => {
-        card = createCard(element, index);
-        cardSection.appendChild(card);
-    });
+  const cardSection = document.getElementById('card-section');
+  myLibrary.forEach((element, index) => {
+    card = createCard(element, index);
+    cardSection.appendChild(card);
+  });
 }
 
 function createCard(element, index) {
-    const card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    card.setAttribute('data-index-number', `${index}`);
+  const card = document.createElement('div');
+  card.setAttribute('class', 'card');
+  card.setAttribute('data-index-number', `${index}`);
 
-    for(property in element) {
-        let el;
-        if(property === 'status') {
-            el = createCheckbox(element[property]);
-        }
-        else {
-            el = createParagraph(element[property]);
-        }
-
-        if(property === 'title') {
-            el.setAttribute('class', 'title');
-        }
-
-        card.appendChild(el);
+  for (property in element) {
+    let el;
+    if (property === 'status') {
+      el = createCheckbox(element[property]);
     }
-    card.appendChild(createEditButton(index));
-    return card;
+    else {
+      el = createParagraph(element[property]);
+    }
+
+    if (property === 'title') {
+      el.setAttribute('class', 'title');
+    }
+
+    card.appendChild(el);
+  }
+  card.appendChild(createEditButton(index));
+  return card;
 }
 
 function createParagraph(value) {
-    const element = document.createElement('p');
-    element.textContent = value;
-    return element;
+  const element = document.createElement('p');
+  element.textContent = value;
+  return element;
 }
 
 function createCheckbox(status) {
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = status;
-    return checkbox;
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = status;
+  return checkbox;
 }
 
 function createEditButton(index) {
-    const button = document.createElement('button');
-    button.textContent = 'Edit';
-    button.setAttribute('data-index-number', `${index}`);
-    button.setAttribute('data-button-type', 'edit');
-    button.addEventListener('click', showForm);
-    return button;
+  const button = document.createElement('button');
+  button.textContent = 'Edit';
+  button.setAttribute('data-index-number', `${index}`);
+  button.setAttribute('data-button-type', 'edit');
+  button.addEventListener('click', showForm);
+  return button;
 }
 
 function showForm() {
-    const form = document.getElementById('add-form');
+  const form = document.getElementById('add-form');
 
-    if(this.dataset.buttonType === 'edit') {
-        const index = this.dataset.indexNumber;
-        const book = myLibrary[index];
-        populateForm(form, book);
-    }
-    
-    form.style.display = 'flex';
+  if (this.dataset.buttonType === 'edit') {
+    const index = this.dataset.indexNumber;
+    const book = myLibrary[index];
+    populateForm(form, book);
+  }
+
+  form.style.display = 'flex';
 }
 
 function hideForm() {
-    const form = document.getElementById('add-form');
-    form.style.display = 'none';
-    form.reset();
+  const form = document.getElementById('add-form');
+  form.style.display = 'none';
+  form.reset();
 }
 
 function populateForm(form, book) {
-    form[0].value = book.title;
-    form[1].value = book.author;
-    form[2].value = book.pages;
-    form[3].checked = book.status;
+  form[0].value = book.title;
+  form[1].value = book.author;
+  form[2].value = book.pages;
+  form[3].checked = book.status;
 }
 
 let myLibrary = [];
@@ -106,10 +106,10 @@ initCards();
 const addBtn = document.getElementById('add-btn');
 addBtn.addEventListener('click', showForm);
 
-const formAddBtn = document.getElementById('form-add-btn');
-formAddBtn.addEventListener('click', _ => {
-    myLibrary.push( addBookToLibrary() );
-    const newCard = createCard(myLibrary.at(-1), myLibrary.length-1);
-    cardSection.appendChild(newCard);
-    hideForm();
-});
+const form = document.getElementById('add-form');
+form.addEventListener('submit', _ => {
+  myLibrary.push(addBookToLibrary());
+  const newCard = createCard(myLibrary.at(-1), myLibrary.length - 1);
+  cardSection.appendChild(newCard);
+  hideForm();
+})
